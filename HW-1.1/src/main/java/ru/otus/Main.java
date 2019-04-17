8975aa6128041f81e350bd86666284cf2f77c3ca
LastHero91 <kam.06@mail.ru>
2019-04-17 22:42:58 +0300

    Multiset jar with dependencies.

--- HW-1.1/src/main/java/ru/otus/Main.java
+++ HW-1.1/src/main/java/ru/otus/Main.java
@@ -1,7 +1,19 @@
 package ru.otus;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!)");
+        Multiset<String> multiset = HashMultiset.create();
+        multiset.add("Multi");
+        multiset.add("Multi");
+        multiset.add("Set");
+        multiset.add("Example");
+        multiset.add("Example");
+        multiset.add("Example");
+        multiset.add("Example");
+
+        System.out.println(multiset);
     }
 }
