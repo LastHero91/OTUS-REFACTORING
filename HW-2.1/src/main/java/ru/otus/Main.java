6152f3a695780b423434fdd7b4c46196ccab1650
LastHero91 <kam.06@mail.ru>
2019-05-05 22:41:47 +0300

    Memory with JOL

--- HW-2.1/src/main/java/ru/otus/Main.java
+++ HW-2.1/src/main/java/ru/otus/Main.java
@@ -1,20 +1,20 @@
 package ru.otus;
 
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class Main {
     private static final int ARRAY_SIZE = 20_000_000;
     public static void main(String[] args) {
-        System.out.println("int: " + measure(()->5));
-        System.out.println("Integer.valueOf: " + measure(()->Integer.valueOf(5)));
-        System.out.println("long: " + measure(()->5L));
+        System.out.println("int: " + measureInt(5));
+        System.out.println("long: " + measureLong(5L));
         System.out.println("int[0]: " + measure(()->new int[0]));
         System.out.println("int[1]: " + measure(()->new int[1]));
-        System.out.println("int[2]: " + measure(()->new int[2]));
-        System.out.println("int[10]: " + measure(()->new int[10]));
         System.out.println("byte[0]: " + measure(()->new byte[0]));
-        System.out.println("char[0]: " + measure(()->new char[0]));
-        System.out.println("boolean[0]: " + measure(()->new boolean[0]));
         System.out.println("new String(new byte[0]): " + measure(()->new String(new byte[0])));
         System.out.println("new String(new char[0])): " + measure(()->new String(new char[0])));
         System.out.println("new String(\"\"): " + measure(()->new String("")));
@@ -22,9 +22,58 @@ public class Main {
         System.out.println("MyClass+int: " + measure(()->new MyClass2()));
         System.out.println("MyClass+long: " + measure(()->new MyClass3()));
         System.out.println("MyClass+boolean: " + measure(()->new MyClass4()));
+        System.out.println("ArrayList<Integer>: " + measure(()->new ArrayList<Integer>()));
         System.out.println(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+        System.out.println("***********************");
+        System.out.println(VM.current().sizeOfField("boolean"));
+        System.out.println(VM.current().sizeOfField("char"));
+        System.out.println(VM.current().sizeOfField("int"));
+        System.out.println(VM.current().sizeOfField("long"));
+        System.out.println(ClassLayout.parseClass(MyClass.class).toPrintable());
+        System.out.println('-');
+        System.out.println(ClassLayout.parseClass(MyClass4.class).toPrintable());
+        System.out.println('-');
+        System.out.println(ClassLayout.parseClass(MyClass5.class).toPrintable());
+        System.out.println('-');
+        System.out.println(ClassLayout.parseClass(MyClass6.class).toPrintable());
+        System.out.println('-');
+        System.out.println(ClassLayout.parseClass(MyClass7.class).toPrintable());
+        System.out.println("***********************");
 
     }
+
+    private static String measureInt(int x){
+        try {
+            long mem = getMem();
+            int array[] = new int[ARRAY_SIZE];
+            long size;
+            for (int i = 0; i < array.length; i++) {
+                array[i] = i;
+            }
+            size = (getMem() - mem) / array.length;
+            return String.valueOf(size);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return String.valueOf(-1);
+    }
+
+    private static String measureLong(long x){
+        try {
+            long mem = getMem();
+            long array[] = new long[ARRAY_SIZE];
+            long size;
+            for (int i = 0; i < array.length; i++) {
+                array[i] = x;
+            }
+            size = (getMem() - mem) / array.length;
+            return String.valueOf(size);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return String.valueOf(-1);
+    }
+
     public static <T> long measure(Supplier<T> supplier) {
         try {
             Object array[] = new Object[ARRAY_SIZE];
@@ -63,5 +112,18 @@ public class Main {
 
     public static class MyClass4{
         private boolean b = true;
+    }
+    public static class MyClass5{
+        private boolean b = true;
+        private boolean boo = true;
+    }
+    public static class MyClass6{
+        private boolean b = true;
+        private int i = 5;
+        private boolean boo = true;
+    }
+    public static class MyClass7{
+        private boolean b = true;
+        private int i = 5;
     }
 }
